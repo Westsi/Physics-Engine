4e768c197962dec809dba95d4a4dd5e817929c71
Westsi <76999267+Westsi@users.noreply.github.com>
2023-03-30 10:31:55 +0100

    started collisions

---
+++ collision.h
@@ -0,0 +1,8 @@
+#include "vector.h"
+#include "coordinate.h"
+#include "particle.h"
+#include <stdbool.h>
+
+bool areColliding(Particle *a, Particle *b);
+
+void applyCollisionForces(Particle *a, Vec3 forces);
